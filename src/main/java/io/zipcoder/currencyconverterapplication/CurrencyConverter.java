a766576bcaf42dfa938f19e747c2a0fd5c7d01eb
hyunjialee <hyjialee@gmail.com>
2023-11-29 21:15:24 -0500

    finished

--- src/main/java/io/zipcoder/currencyconverterapplication/CurrencyConverter.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/CurrencyConverter.java
@@ -2,6 +2,18 @@ package io.zipcoder.currencyconverterapplication;
 
 public class CurrencyConverter {
     public static Double convert(Double amountOfBaseCurrency, ConvertableCurrency sourceCurrencyType, CurrencyType targetCurrencyType) {
-        return sourceCurrencyType.convert(targetCurrencyType) * amountOfBaseCurrency;
+        return (targetCurrencyType.getRate()
+                /sourceCurrencyType
+                .getConvertedCurrency()
+                .getRate()) * amountOfBaseCurrency;
+
+        /**
+         Base currency rate / Foreign currency rate  = EXCHANGE RATE
+
+         RATE * AMOUNT = the converted amount
+
+         Convertible currency - CURRENCY BY ENUM -> RATE
+
+        */
     }
 }
--- src/main/java/io/zipcoder/currencyconverterapplication/currencies/USDollar.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/currencies/USDollar.java
@@ -1,6 +1,11 @@
 package io.zipcoder.currencyconverterapplication.currencies;
 
 import io.zipcoder.currencyconverterapplication.ConvertableCurrency;
+import io.zipcoder.currencyconverterapplication.CurrencyType;
 
 public class USDollar implements ConvertableCurrency {
+    @Override
+    public CurrencyType getConvertedCurrency() {
+        return CurrencyType.US_DOLLAR;
+    }
 }
